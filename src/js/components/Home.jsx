d7527b5ceb6689ccd5f9331ff7b1fd92fa673c8f
RdeRausseo <amrausseo20@gmail.com>
2025-08-25 23:28:50 +0000

    Repositorio montado, useEffect cargando la api sin errores

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,59 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [userToDo, setUser] = useState({ name: "", todos: [] });
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  useEffect(() => {
+    const getUserTodo = async () => {
+      try {
+        const request = await fetch(
+          "https://playground.4geeks.com/todo/users/rder",
+          {
+            method: "GET",
+            headers: {
+              Accept: "application/json",
+            },
+          }
+        );
+
+        if (!request.ok) {
+          throw new Error(`Error en el request: ${request.status} - ${request.statusText}`);
+        }
+
+        const response = await request.json();
+        console.log(response);
+        setUser({ name: response.name, todos: response.todos });
+      } catch (error) {
+        console.error("Error: ", error);
+      }
+    };
+    getUserTodo();
+  }, []);
+
+  useEffect(() => {
+    console.log(userToDo);
+  }, []);
+
+  return (
+    <div className="text-center">
+      <h1 className="text-center mt-5">Hello Rigo!</h1>
+      <p>
+        <img src={rigoImage} />
+      </p>
+      <a href="#" className="btn btn-success">
+        If you see this green button... bootstrap is working...
+      </a>
+      <p>
+        Made by <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+        love!
+      </p>
+    </div>
+  );
 };
 
 export default Home;
