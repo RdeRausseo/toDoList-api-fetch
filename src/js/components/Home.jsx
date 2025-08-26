7a3d5ed52a90321e00c95171b1db0beee2a42506
RdeRausseo <amrausseo20@gmail.com>
2025-08-26 02:00:05 +0000

    Funcionalidad para añadir tareas a la API y luego renderizarlas, implementada con exito

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -7,51 +7,112 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
   const [userToDo, setUser] = useState({ name: "", todos: [] });
+  const [inputTask, setInputTask] = useState("");
+  const [inputErrors, setErrors] = useState([]);
+
+  const getUserTodo = async () => {
+    try {
+      const request = await fetch(
+        "https://playground.4geeks.com/todo/users/rder",
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+          },
+        }
+      );
+
+      if (!request.ok) {
+        throw new Error(
+          `Error en el request: ${request.status} - ${request.statusText}`
+        );
+      }
+
+      const response = await request.json();
+      console.log(response);
+      setUser({ name: response.name, todos: response.todos });
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+  };
 
   useEffect(() => {
-    const getUserTodo = async () => {
-      try {
-        const request = await fetch(
-          "https://playground.4geeks.com/todo/users/rder",
-          {
-            method: "GET",
-            headers: {
-              Accept: "application/json",
-            },
-          }
-        );
-
-        if (!request.ok) {
-          throw new Error(`Error en el request: ${request.status} - ${request.statusText}`);
-        }
-
-        const response = await request.json();
-        console.log(response);
-        setUser({ name: response.name, todos: response.todos });
-      } catch (error) {
-        console.error("Error: ", error);
-      }
-    };
     getUserTodo();
   }, []);
 
-  useEffect(() => {
-    console.log(userToDo);
-  }, []);
+  const addTask = async () => {
+    if (inputTask.trim() === "") {
+      setErrors([...inputErrors, "No puedes agregar tareas vacías"]);
+      return;
+    }
+
+    const newTask = { label: inputTask, is_done: true };
+
+    setErrors([]);
+
+    try {
+      const request = await fetch(
+        "https://playground.4geeks.com/todo/todos/rder",
+        {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+            accept: "application/json",
+          },
+          body: JSON.stringify(newTask),
+        }
+      );
+
+      if (request.ok) {
+        getUserTodo();
+        setInputTask("");
+      } else {
+        throw new Error(
+          `Error en el request: ${request.status} - ${request.statusText}`
+        );
+      }
+    } catch (error) {
+      setErrors([
+        "Revisa tu conexión a internet, error al conectar con el servidor",
+      ]);
+      console.error("Error: ", error);
+    }
+  };
+
+  function handleKeyDown(e) {
+    if (e.key === "Enter") {
+      addTask();
+      console.log("Enter Presionado");
+    }
+  }
 
   return (
-    <div className="text-center">
-      <h1 className="text-center mt-5">Hello Rigo!</h1>
-      <p>
-        <img src={rigoImage} />
-      </p>
-      <a href="#" className="btn btn-success">
-        If you see this green button... bootstrap is working...
-      </a>
-      <p>
-        Made by <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-        love!
-      </p>
+    <div className="container text-center p-5">
+      <h1 className="display-3"> ToDo List de: {userToDo.name} </h1>
+      <ul className="list-group my-2">
+        {inputErrors?.map((error, index) => (
+          <li key={index}> {error} </li>
+        ))}
+      </ul>
+      <ul className="list-group">
+        <li className="list-group-item">
+          <input
+            className="form-control fs-3 text-center"
+            name="task"
+            onChange={(e) => {
+              setInputTask(e.target.value);
+            }}
+            value={inputTask}
+            onKeyDown={handleKeyDown}
+            placeholder="What do you need to be ?"
+          />
+        </li>
+        {userToDo.todos?.map((todos) => (
+          <li key={todos.id} className="list-group-item fs-3">
+            {todos.label}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
